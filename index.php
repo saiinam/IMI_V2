17c27910571f4db32d97f4b4ab31a8bd61bf1006
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-19 23:21:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,40 @@ Kotchakorn Somsanog
 <!doctype html>
 <html lang="en">
   <head>
-
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.js"
+    integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
   </head>
+
   <body>
-    <div class="row">
+    <h1>Hello world!</h1>
+    
 
-        <iframe frameborder="0" style="height:400px;width:600px"
-        src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
-
-        </div>
-
-    </div>
-
-    <h1>Hello, world!</h1>
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
+    
   </body>
-    <script>
-        $(()=>{
-            alert("hello");
-            
-        });
+  <script>
+      $(()=>{
+          //alert("Hello");
+          let url="https://api.thingspeak.com/channels/1458410/feeds.json?results=2";
+          $.getJSON(url)
+            .done(function(data){
+              console.log(data);
+            })
+            .fail(function(error){
 
-    </script>
+            });
+      });
+
+
+  </script>
 </html>
