1d4fc9f46719ee6f14c3bb91d4c8743f1c0e1467
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-19 11:36:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ Kotchakorn Somsanog
   </head>
   <body>
     <div class="row">
-        <div class="col-4">
+
         <iframe frameborder="0" style="height:400px;width:600px"
         src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
 
